011c92a88ea44161774c632e3c43bc1c00caa1fe
Alex Khanin <akh.alex.khan@gmail.com>
2021-11-09 23:43:37 +0200

    add panic recover on log wrapper

--- golang/logWrapper.go
+++ golang/logWrapper.go
@@ -1,21 +1,36 @@
 package golang
 
-//LoggerWrapper - обертка, адаптер для любого типа логера
-type LoggerWrapper func(string, ...interface{})
+import (
+	"fmt"
+	"os"
+)
 
-// Printf - реализация интерфейса необходимого для работы retryablehttp
-func (log LoggerWrapper) Printf(str string, args ...interface{}) {
-	log(str, args...)
-}
+// //LoggerWrapper - обертка, адаптер для любого типа логера
+// type LoggerWrapper func(string, ...interface{})
+
+// // Printf - реализация интерфейса необходимого для работы retryablehttp
+// func (log LoggerWrapper) Printf(str string, args ...interface{}) {
+// 	log(str, args...)
+// }
 
 type LoggerWriterWraper func(string)
 
 func (log LoggerWriterWraper) Write(buf []byte) (int, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("%v", err))
+		}
+	}()
 	log(string(buf))
 	return len(buf), nil
 }
 
 func (log LoggerWriterWraper) WriteString(buf string) (int, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("%v", err))
+		}
+	}()
 	log(buf)
 	return len(buf), nil
 }
